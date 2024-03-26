bd09e93ba0959f21b96bb3289718c872481588f9
KClower <kclower3@gmail.com>
2024-03-25 19:11:08 -0500

    finished project

--- src/index.js
+++ src/index.js
@@ -40,3 +40,71 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const links = document.querySelectorAll("nav a");
+for(let i = 0; i < links.length; i++){
+  const linkText = siteContent.nav[`nav-item-${i+1}`];
+  links[i].textContent = linkText;
+  links[i].classList.add("italic")
+}
+
+const logoImg = document.querySelector(".logo")
+logoImg.src = siteContent.images["logo-img"]
+
+const ctaHeader = document.querySelector(".cta h1");
+ctaHeader.textContent = siteContent.cta.h1
+
+const ctaButton = document.querySelector(".cta button");
+ctaButton.textContent = siteContent.cta.button
+
+const ctaImg = document.querySelector("#cta-img")
+ctaImg.src = siteContent.images["cta-img"]
+
+const accentImg = document.querySelector("#middle-img")
+accentImg.src = siteContent.images["accent-img"]
+
+const mainContent = siteContent["main-content"]
+const textContentCollection = document.querySelectorAll(".text-content");
+const[featuresContainer, aboutContainer, servicesContainer, productContainer, visionContainer] = textContentCollection
+
+const featureHeader = featuresContainer.children[0];
+const featureText = featuresContainer.children[1];
+featureHeader.textContent = mainContent["features-h4"]
+featureText.textContent = mainContent["features-content"]
+
+const [aboutHeader, aboutText] = aboutContainer.children
+aboutHeader.textContent = mainContent["about-h4"]
+aboutText.textContent = mainContent["about-content"]
+
+const [servicesHeader, servicesText] = servicesContainer.children
+servicesHeader.textContent = mainContent["services-h4"]
+servicesText.textContent = mainContent["services-content"]
+
+const [productHeader, productText] = productContainer.children
+productHeader.textContent = mainContent["product-h4"]
+productText.textContent = mainContent["product-content"]
+
+const [visionHeader, visionText] = visionContainer.children
+visionHeader.textContent = mainContent["vision-h4"]
+visionText.textContent = mainContent["vision-content"]
+
+const contactContent = siteContent["contact"]
+const contactContainer = document.querySelector(".contact");
+const [contactHeader, address, phone, email] = contactContainer.children
+contactHeader.textContent = contactContent["contact-h4"]
+address.textContent = contactContent.address
+phone.textContent = contactContent.phone
+email.textContent = contactContent.email
+
+
+const footerContent = siteContent.footer
+const footerLink = document.querySelector("footer a");
+footerLink.textContent = footerContent.copyright
+footerLink.classList.add("bold")
+
+
+
+
+console.log(textContentCollection)
+
+
